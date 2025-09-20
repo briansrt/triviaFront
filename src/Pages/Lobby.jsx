5fbc695cfbc686febfc46f6decac5e269a9f9e53
briansrt <brianstevenriofriotrochez@hotmail.com>
2025-09-20 15:33:52 -0500

    asd

--- src/Pages/Lobby.jsx
+++ src/Pages/Lobby.jsx
@@ -137,7 +137,7 @@ export default function Lobby() {
             </div>
 
 
-            {currentRoom.players.length === 2 && (
+            {currentRoom.players.length === 6 && (
               <button
                 onClick={handleStartGame}
                 className="bg-red-500 text-white px-4 py-2 rounded-lg mt-4"
