59e359806a7f583ced7ef24e6f7cd6a106a8a03b
briansrt <brianstevenriofriotrochez@hotmail.com>
2025-09-27 13:05:12 -0500

    probando cloudfront

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
+  throw new Error('Missing Publishable Key of CLERK')
 }
 
 createRoot(document.getElementById('root')).render(
